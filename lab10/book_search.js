8c7e95c7fd33b047ebd17207e82bb9768e75c7f0
wogns3623 <wogns3623@gmail.com>
2019-12-06 16:40:43 +0900

    Add lab10 Folder

---
+++ lab10/book_search.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+  $("b_xml").onclick = function() {
+    //construct a Prototype Ajax.request object
+    console.log({
+      method: "get",
+      parameters: { category: getCheckedRadio($$("input")) },
+      onSuccess: showBooks_XML,
+      onFailure: ajaxFailed
+    });
+    new Ajax.Request("", {
+      method: "get",
+      parameters: { category: getCheckedRadio($$("input")) },
+      onSuccess: showBooks_XML,
+      onFailure: ajaxFailed
+    });
+  };
+  $("b_json").onclick = function() {
+    //construct a Prototype Ajax.request object
+  };
+};
+
+function getCheckedRadio(radio_button) {
+  console.log(radio_button);
+  for (var i = 0; i < radio_button.length; i++) {
+    if (radio_button[i].checked) {
+      return radio_button[i].value;
+    }
+  }
+  return undefined;
+}
+
+function showBooks_XML(ajax) {
+  alert(ajax.responseText);
+}
+
+function showBooks_JSON(ajax) {
+  alert(ajax.responseText);
+}
+
+function ajaxFailed(ajax, exception) {
+  var errorMessage = "Error making Ajax request:\n\n";
+  if (exception) {
+    errorMessage += "Exception: " + exception.message;
+  } else {
+    errorMessage +=
+      "Server status:\n" +
+      ajax.status +
+      " " +
+      ajax.statusText +
+      "\n\nServer response text:\n" +
+      ajax.responseText;
+  }
+  alert(errorMessage);
+}
